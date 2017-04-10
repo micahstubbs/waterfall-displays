41e638f6253aebd4c6e5bc6929e0391a347d3a8d
Micah Stubbs <micah.stubbs@gmail.com>
2017-04-10 16:42:08 -0700

    eslint --fix vis.js

--- 02/vis.js
+++ 02/vis.js
@@ -1,78 +1,76 @@
-    // get the context from the canvas to draw on
-    canvasElement = document.getElementById("canvas");
-    ctx = canvasElement.getContext("2d");
+/* global document AudioContext chroma navigator window */
 
-    // create a temporary canvas to use for copying
-    const tempCanvas = document.createElement("canvas");
+// get the context from the canvas to draw on
+const canvasElement = document.getElementById('canvas');
+const ctx = canvasElement.getContext('2d');
 
-    const tempCtx = tempCanvas.getContext("2d");
-    let timer;
-    tempCanvas.width=512;
-    tempCanvas.height=800;
+// create a temporary canvas to use for copying
+const tempCanvas = document.createElement('canvas');
 
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    const context = new AudioContext();
-    const analyser = context.createAnalyser();
+const tempCtx = tempCanvas.getContext('2d');
+let timer;
+tempCanvas.width = 512;
+tempCanvas.height = 800;
 
-    // used for color distribution
-    const hot = new chroma.ColorScale({
-        colors: [ '#000000', '#0B16B5', '#FFF782', '#EB1250' ],
-        positions: [ 0, 0.4, 0.68, 0.85 ],
-        mode:'rgb',
-        limits:[0, 300]   
-    });
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+const context = new AudioContext();
+const analyser = context.createAnalyser();
 
-    navigator.webkitGetUserMedia({ audio: true }, stream => {
-        const source = context.createMediaStreamSource(stream);
-        source.connect(analyser);
-        analyser.connect(context.destination);
+// used for color distribution
+const hot = new chroma.ColorScale({
+  colors: ['#000000', '#0B16B5', '#FFF782', '#EB1250'],
+  positions: [0, 0.4, 0.68, 0.85],
+  mode: 'rgb',
+  limits: [0, 300],
+});
 
-        setInterval(() => {
-            const array = new Uint8Array(analyser.frequencyBinCount);
-            analyser.getByteFrequencyData(array);
+navigator.webkitGetUserMedia({ audio: true }, (stream) => {
+  const source = context.createMediaStreamSource(stream);
+  source.connect(analyser);
+  analyser.connect(context.destination);
 
-            // draw the spectrogram
-            drawSpectrogram(array);
+  setInterval(() => {
+    const array = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(array);
 
-        }, 10);
-                    
-    }, () => { });
+        // draw the spectrogram
+    drawSpectrogram(array);
+  }, 10);
+}, () => { });
 
-    // array is an array of 512 numbers
-    function drawSpectrogram(array) {
+// array is an array of 512 numbers
+function drawSpectrogram(array) {
+    // copy the current canvas onto the temp canvas
+  const canvas = document.getElementById('canvas');
 
-        // copy the current canvas onto the temp canvas
-        const canvas = document.getElementById("canvas");
+    // Position the image on the canvas, and specify width and
+    // height of the image context.drawImage(img,x,y,width,height);
+  tempCtx.drawImage(canvas, 0, 0, 512, 800);
 
-        // Position the image on the canvas, and specify width and 
-        //height of the image context.drawImage(img,x,y,width,height);
-        tempCtx.drawImage(canvas, 0, 0, 512, 800);
+    // iterate over the elements from the array
+  for (let i = 0; i < array.length; i += 1) {
+        // draw each pixel with the specific color
+    const value = array[i];
+    ctx.fillStyle = hot.getColor(value).hex();
 
-        // iterate over the elements from the array
-        for (let i = 0; i < array.length; i++) {
-            // draw each pixel with the specific color
-            const value = array[i];
-            ctx.fillStyle = hot.getColor(value).hex();
+        // draw the line at the bottom of the canvas
+        // context.fillRect(x,y,width,height);
+    ctx.fillRect(i, 0, 1, 1);
+  }
 
-            // draw the line at the bottom of the canvas
-            // context.fillRect(x,y,width,height);
-            ctx.fillRect( i, 0, 1, 1);
-        }
+    // set translate on the canvas: context.translate(x,y);
+  ctx.translate(0, 1);
+    // draw the copied image
+    // context.drawImage(img,x,y,width,height);
+  ctx.drawImage(tempCanvas, 0, 0, 512, 800, 0, 0, 512, 800);
 
-        // set translate on the canvas: context.translate(x,y);
-        ctx.translate(0, 1);
-        // draw the copied image
-        // context.drawImage(img,x,y,width,height);
-        ctx.drawImage(tempCanvas, 0, 0, 512, 800, 0, 0, 512, 800);
-
-        // reset the transformation matrix
-        // context.setTransform(a,b,c,d,e,f);
-        // a  Horizontal scaling
-        // b  Horizontal skewing
-        // c  Vertical skewing
-        // d  Vertical scaling
-        // e  Horizontal moving
-        // f  Vertical moving       
-        ctx.setTransform(1, 0, 0, 1, 0, 0);
-
-    }
+    // reset the transformation matrix
+    // context.setTransform(a,b,c,d,e,f);
+    // a  Horizontal scaling
+    // b  Horizontal skewing
+    // c  Vertical skewing
+    // d  Vertical scaling
+    // e  Horizontal moving
+    // f  Vertical moving
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+}
